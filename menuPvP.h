4b3832d6ed4a5ebc3376071c4998dca360754e1b
JonathanMolina00141225 <00141225@uca.edu.sv>
2025-06-18 11:51:56 -0600

    :memo:

---
+++ menuPvP.h
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int menuPvP(){
+    do
+{
+    cout<<"NOW WE'RE GOING TO SEE THE FIGHT, Player 1 vs Player 2 \n";
+    cout<<"What do you want to do? \n";
+    cout<<"1. Fight \n";
+    cout<<"2. Backpack \n";
+    cout<<"3. Pokemon \n";
+    cout<<"4. Go away \n";
+    cin>>PvPmenu;
+} while (PvPmenu!=4);
+cout<<"You left the fight \n";
+}
